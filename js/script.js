19e4a11502d62c87c1a68fa7d27b10a12693bc54
Lin Lin <krislinnaing@gmail.com>
2024-05-18 18:56:01 +0630

    slideshow

--- js/script.js
+++ js/script.js
@@ -9,3 +9,43 @@ $(function() {
     document.getElementById('event-month').innerHTML = month;
     document.getElementById('event-year').innerHTML = year;
 });
+
+ // Slideshow
+ let slidePosition = 1;
+ SlideShow(slidePosition);
+
+ // Prev/Next buttons
+ function plusSlides(n) {
+     SlideShow(slidePosition += n);
+ }
+
+ // Counter
+ function slideCounter(n) {
+     SlideShow(slidePosition = n);
+ }
+
+ function SlideShow(n) {
+     let slides = document.getElementsByClassName("slide");
+     let pagers = document.getElementsByClassName("pager");
+     // Loop Start
+     if (n > slides.length) {
+         slidePosition = 1;
+         console.log("loop start");
+     }
+     //Loop End
+     if (n < 1) {
+         slidePosition = slides.length;
+         console.log("loop end");
+
+     }
+     // Hide all slides and pager
+     for (let i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+     }
+     for (let i = 0; i < pagers.length; i++) {
+         pagers[i].className = pagers[i].className.replace(" enable", "");
+     }
+     // Show current slide and pager
+     slides[slidePosition-1].style.display = "block";
+     pagers[slidePosition-1].className += " enable";
+ } 
