a927360b61b535aa8ceb9470f3d15799ffcdcc96
Lin Lin <krislinnaing@gmail.com>
2025-01-24 16:18:50 +0700

    updated html structure, css and js for slide in slide out menu

--- js/script.js
+++ js/script.js
@@ -1,30 +1,50 @@
 (function($) {
-    $(document).on('click', '.down-arrow', function(evt) {
-       evt.preventDefault(); // Prevent default hash behavior
+    $(document).on('click', '.down-arrow', () => {
+       //evt.preventDefault();
  
-       const targetId = $(this).attr('href'); // Get the href (e.g., #section1)
-       const targetElement = $(targetId); // Target element
+       const targetId = $(this).attr('href'); 
+       const targetElement = $(targetId);
  
        if (targetElement.length) {
           $('html, body').animate({
              scrollTop: targetElement.offset().top // Scroll to the element
-          }, 100, function() {
-             // Remove hash from the URL after scrolling
+          }, 100, 
+          /*
+          function() {
              history.replaceState(null, null, ' ');
-          });
+          }
+             */
+        );
        }
     });
  })(window.jQuery);
 
+ 
+//Menu slide in and slide out
+const menu = document.getElementById("menu");
+const navBar = document.getElementById("navbar");
+const menuItems = document.querySelectorAll(".nav-menu ul li a");
+
+menu.addEventListener("click", () => {
+   menu.classList.toggle("visible");
+   navBar.classList.toggle("sideMenu");
+});
+
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        menu.classList.remove("visible"); 
+        navBar.classList.remove("sideMenu");
+    });
+});
 
 $(function() {
     let eventDate = moment().add(3, 'day').add(1, 'months');
     let day = eventDate.format("DD");
     let month = eventDate.format("MM");
     let year = eventDate.format("YYYY");
-    document.getElementById('event-day').innerHTML = day;
-    document.getElementById('event-month').innerHTML = month;
-    document.getElementById('event-year').innerHTML = year;
+    document.getElementById('event-day').innerText = day;
+    document.getElementById('event-month').innerText = month;
+    document.getElementById('event-year').innerText = year;
 });
 
  // Slideshow
@@ -47,12 +67,10 @@ $(function() {
      // Loop Start
      if (n > slides.length) {
          slidePosition = 1;
-         console.log("loop start");
      }
      //Loop End
      if (n < 1) {
          slidePosition = slides.length;
-         console.log("loop end");
 
      }
      // Hide all slides and pager
@@ -66,4 +84,8 @@ $(function() {
      slides[slidePosition-1].style.display = "block";
      pagers[slidePosition-1].className += " enable";
  } 
+
+
+
+
 
